3cdd511378845f63ae244831bbdb4c670fb10fbf
RachelKolk <rach.m.obrien@gmail.com>
2019-08-08 15:01:03 -0400

    fixed error in create user method

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(username: params[:session][:username])
     if user && user.authenticate(params[:session][:password])
-      session[:user_id] = user.user_id
+      session[:user_id] = user.id
       flash[:success] = "You have successfully logged in"
       redirect_to root_path
     else
